f4f0582000923e9d2a7214decc12bd9623969717
hieunghiadoan <126639448+hieunghiadoan@users.noreply.github.com>
2023-05-15 21:10:18 +1000

    modified the code and done with module challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,80 +1,53 @@
-#PyBank analysis script
-#
-# Import the os module and the csv module
-
 import os
 import csv
 
-# Set path for data
-csvpath = os.path.join("Users", "nghiadoanhieu", "Monash Bootcamp", "Homework" ,"Resources", "budget_data.csv")
-pathout = os.path.join("Users", "nghiadoanhieu", "Monash Bootcamp", "Homework" "Analysis", "budget_analysis.txt")
+#path to collect data from the resources folder
 
-#Lists and variables to store data
-
-date = []
-monthly_pnl = []
-months = 0 
-total = 0
-prev_rev = 0
-change = 0
-total_change = 0
-inc = ["", 0]
-dec = ["", 0]
-
-# Open path
-
-with open(csvpath, newline ="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter= ',')
-    csv_header = next(csvreader)
+budget_data_csv = os.path.join("Resources/budget_data.csv")
 
 
-    # Read each row of data after the header and assign variables
-    for row in csvreader:
-        date.append(row[0])
-        monthly_pnl.append(row[1])
-# Create script to calculate the following:
+#Create empty list
+month_count=[]
+profit_losses=[]
+change=[]
 
-        #The total number of months included in the dataset
-        months += 1
-        total += int(row[1])
+with open(budget_data_csv) as csv_file:
+    csv_reader=csv.reader(csv_file, delimiter=",")
+    header = next(csv_reader)
+    
+    for row in csv_reader:
+        month_count.append(str(row[0]))
+        profit_losses.append(int(row[1]))
+    
+   
+    for i in range(len(profit_losses)-1):
+       
+        change.append(profit_losses[i+1]- profit_losses[i])
+    
+    Increase=max(change)
+    Decrease=min(change)
+    month_increase=change.index(max(change))+1
+    month_decrease=change.index(min(change))+1    
+    
+print("Financial Analysis")
+print("-----------------------------------------------")
+print(f'Total Months: {str(len(month_count))}')
+print(f'Total: ${sum(profit_losses)}')
+print(f"Average Change: {round(sum(change)/ len(change),2)}")
+print(f"Greatest Increase in Profits: {month_count[month_increase]} (${(str(Increase))})")
+print(f"Greatest Decrease in Profits: {month_count[month_decrease]} (${(str(Decrease))})")
 
-        #Evaluate change
-        change = int(row[1]) - prev_rev
-        if prev_rev == 0:
-            change = 0
-        prev_rev = int(row[1])
-        total_change += change
-
-        #Evaluate Increase
-        if change > int(inc[1]):
-            inc[1] = change
-            inc[0] = row[0]
-
-        #Evaluate Decrease
-        if change < int(dec[1]):
-            dec[1] = change
-            dec[0] = row[0]
-
-
-    total_change = total_change / (months - 1)
-
-
-# Print the analysis to the terminal
-print("\n\nFinancial Analysis")
-print("----------------------------")
-print(f"Months total in Analysis:  {months}")
-print(f"Total:  ${total}")
-print(f"Average Change: ${total_change:.2f}")
-print(f"Greatest Increase in Profits: {inc[0]} (${inc[1]})")
-print(f"Greatest Decrease in Profits: {dec[0]} (${dec[1]})")
-
-
- # Export a text file with the results to Analysis folder
-with open(pathout, "w") as results:
-     results.write("\n\nFinancial Analysis\n")
-     results.write("----------------------------\n")
-     results.write(f"Months total in Analysis:  {months}\n")
-     results.write(f"Total:  ${total}\n")
-     results.write(f"Average Change: ${total_change:.2f}\n")
-     results.write(f"Greatest Increase in Profits: {inc[0]} (${inc[1]})\n")
-     results.write(f"Greatest Decrease in Profits: {dec[0]} (${dec[1]})\n")
+pyBank= os.path.join(".", 'pyBank.txt')
+with open('pyBank.txt' , 'w') as text:
+    text.write("Financial Analysis\n")
+    text.write("-----------------------------------------------\n")
+    text.write(f'Total Months: {str(len(month_count))}')
+    text.write("\n")
+    text.write(f'Total: ${sum(profit_losses)}')
+    text.write("\n")
+    text.write(f"Average Change: {round(sum(change)/ len(change),2)}")
+    text.write("\n")
+    text.write(f"Greatest Increase in Profits: {month_count[month_increase]} (${(str(Increase))})")
+    text.write("\n")
+    text.write(f"Greatest Decrease in Profits: {month_count[month_decrease]} (${(str(Decrease))})")
+               
